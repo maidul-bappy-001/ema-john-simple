ca57437872cee3b569615587178fdf63415d58e6
Maidul <maidul.bappy.001@gmail.comm>
2021-04-22 01:34:57 +0600

    Ema john Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import Shop from './Components/Shop/Shop'
 
 export default function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
+
     </div>
   )
 }
